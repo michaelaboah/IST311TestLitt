d5ec8fc576236267aa5bf0a019d66bb9f32b6037
michaelaboah <66381519+michaelaboah@users.noreply.github.com>
2022-03-27 21:14:45 -0400

    Init

---
+++ src/main/java/Controller/cardController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import View.loginScreen;
+import View.studentMenu;
+import Model.User;
+import Model.UserList;
+import Controller.studentController;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import View.cardMenu;
+
+/**
+ *
+ * @author Anita
+ */
+public class cardController {
+    cardMenu crd = new cardMenu();
+    public cardController(){
+       /* String[] cardFront = { "A", "B"};
+        String[] cardBack = { "A Back", "B Back"};
+        */
+       crd.setVisible(true);
+       
+        
+        
+        
+    }
+}
---
+++ src/main/java/Model/Cards.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author miker
+ */
+public class Cards {
+    
+    String cardFront;
+    String cardBack;
+    
+     public Cards(){
+        cardFront = "";
+        cardBack = "";      
+    }
+    
+    public Cards(String cardFront, String cardBack){
+        this.cardFront = cardFront;
+        this.cardBack = cardBack;
+    }
+    
+    
+}
